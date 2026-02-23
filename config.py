332e57b19a1b463b5d7c3a7775cfad34c76fcf4f
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2026-02-23 08:21:32 +0300

    Delete config.py

--- config.py
+++
@@ -1,9 +0,0 @@
-import os
-from dotenv import load_dotenv
-
-load_dotenv()
-
-TOKEN = os.getenv("TOKEN")
-USERNAME = os.getenv("USERNAME")
-DATABASE_URL = os.getenv("DATABASE_URL")
-ALLOWED_CHAT_ID = os.getenv("ALLOWED_CHAT_ID")
