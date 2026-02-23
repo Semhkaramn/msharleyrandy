12dc1ee1f3635bdde064bfe95ec96688e21bc56d
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2026-02-23 08:20:57 +0300

    Delete helpers directory

--- helpers/__init__.py
+++
@@ -1,6 +0,0 @@
-from .locale import get_text
-from .check_admin import is_admin
-from .chat_restriction import check_chat_restriction
-from .message_counter import handle_message_count
-
-__all__ = ['get_text', 'is_admin', 'check_chat_restriction', 'handle_message_count']
