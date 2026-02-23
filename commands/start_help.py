019028538333eec99e4bf89b4ce76d91b25dab07
Semhkaramn <158337999+Semhkaramn@users.noreply.github.com>
2026-02-23 08:20:46 +0300

    Delete commands directory

--- commands/start_help.py
+++
@@ -1,9 +0,0 @@
-from telegram import Update
-from telegram.ext import ContextTypes
-from helpers.locale import get_text
-
-async def start_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(get_text("start"))
-
-async def help_command(update: Update, context: ContextTypes.DEFAULT_TYPE):
-    await update.message.reply_text(get_text("help"))
